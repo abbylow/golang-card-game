53295fc5e67a1e94aa1647b13a0eeedb3134df6d
abby <abby.low@bybit.com>
2021-01-18 19:58:34 +0800

    complete newDeck function

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	// append: doesn't modify the existing slice, create a new slice and assign to cards
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
 }
